f3078c56029e7b6adc2631698532f1f94d611d5f
dan4451 <49764053+dan4451@users.noreply.github.com>
2022-03-08 16:50:09 -0800

    email script

---
+++ Scripts/New/Scripts/SendEmailDirectSend.ps1
@@ -0,0 +1,14 @@
+﻿## Build parameters
+$mailParams = @{
+    SmtpServer                 = 'trilinkbiotech-com.mail.protection.outlook.com'
+    Port                       = '25'
+    UseSSL                     = $true   
+    From                       = 'dfrancia@trilinkbiotech.com'
+    To                         = 'dfrancia@trilinkbiotech.com'
+    Subject                    = "Direct Send $(Get-Date -Format g)"
+    Body                       = 'This is a test email using Direct Send'
+    DeliveryNotificationOption = 'OnFailure', 'OnSuccess'
+}
+
+## Send the email
+Send-MailMessage @mailParams
---
+++ Scripts/New/Scripts/SendEmailSMTPAuthenticated.ps1
@@ -0,0 +1,18 @@
+﻿# Get the credential
+$credential = Get-Credential
+
+## Define the Send-MailMessage parameters
+$mailParams = @{
+    SmtpServer                 = 'smtp.office365.com'
+    Port                       = '587' # or '25' if not using TLS
+    UseSSL                     = $true ## or not if using non-TLS
+    Credential                 = $credential
+    From                       = 'dfrancia@trilinkbiotech..com'
+    To                         = 'dfrancia@trilinkbiotech.com'
+    Subject                    = "SMTP Client Submission - $(Get-Date -Format g)"
+    Body                       = 'This is a test email using SMTP Client Submission'
+    DeliveryNotificationOption = 'OnFailure', 'OnSuccess'
+}
+
+## Send the message
+Send-MailMessage @mailParams
