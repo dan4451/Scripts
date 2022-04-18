126a278ea5d283fc321934fe1bc17a5f3331cf4c
dan4451 <49764053+dan4451@users.noreply.github.com>
2022-04-18 16:31:57 -0700

    Create copyPS1LocalAndExecute.ps1

---
+++ Scripts/MDT/copyPS1LocalAndExecute.ps1
@@ -0,0 +1,3 @@
+﻿powershell.exe -WindowStyle Minimized -ExecutionPolicy Bypass -Command "Copy-Item '%DEPLOYROOT%\Applications\DisableRestore\systemrestoredisable.ps1' -destination C:\Windows\Temp;  
+C:\Windows\Temp\systemrestoredisable.ps1; 
+Remove-Item C:\Windows\temp\*.ps1 -Force"
