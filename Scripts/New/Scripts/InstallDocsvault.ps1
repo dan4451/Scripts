8982cddade40dc0402f904b5827c9a95cb94ba27
dan4451 <49764053+dan4451@users.noreply.github.com>
2022-06-01 16:42:27 -0700

    InstallDS

---
+++ Scripts/New/Scripts/InstallDocsvault.ps1
@@ -0,0 +1,19 @@
+﻿$TrueHash = "AF2EBDD6FC023B33938E622ACF9BF3AD21A0E494DC6BD84323CA14FAC926B500"
+$test = Test-Path -Path "C:\Program Files (x86)\Docsvault SB\Client\dvclient.exe"
+
+function install-DSclient 
+{
+    get-process -name explorer | Stop-Process -Force
+    msiexec /i "\\trilinksd.local\SysVol\trilinksd.local\Apps\Docsvault_SB_Client_v13.5.msi" /quiet
+}
+
+if($test)
+{
+    $Hash = Get-FileHash "C:\Program Files (x86)\Docsvault SB\Client\dvclient.exe"
+    if($Hash.Hash -ne $TrueHash)
+    {
+        install-DSclient
+    }
+
+}
+else{exit}
---
+++ Scripts/New/Scripts/InstallDocsvaultOLD.ps1
@@ -0,0 +1,12 @@
+﻿$TrueHash = "AF2EBDD6FC023B33938E622ACF9BF3AD21A0E494DC6BD84323CA14FAC926B500"
+$Hash = Get-FileHash "C:\Program Files (x86)\Docsvault SB\Client\dvclient.exe"
+$ReportedHash = $Hash.Hash
+$test = Test-Path -Path "C:\Program Files (x86)\Docsvault SB\Client\dvclient.exe"
+if($Hash = $null){exit}
+else{
+    if($ReportedHash -ne $TrueHash){
+
+    get-process -name explorer | Stop-Process -Force
+    msiexec /i "\\trilinksd.local\SysVol\trilinksd.local\Apps\Docsvault_SB_Client_v13.5.msi" /quiet
+    }
+}
