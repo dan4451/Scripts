c84b05c6953d1ff5854f6b98056ed17bdda997fd
dan4451 <49764053+dan4451@users.noreply.github.com>
2022-06-13 10:50:39 -0700

    update esxi

--- Scripts/scripts/connectToESXI.ps1
+++ Scripts/scripts/connectToESXI.ps1
@@ -1,1 +1,8 @@
-﻿Connect-VIServer TLB-HOST04
+﻿#Connects to the Hypervisor
+Connect-VIServer TLB-HOST04
+
+$name = SD-VM-DAN04
+$targetVMHost = Get-VMHost -Name TLB-HOST04
+
+#Creates a VM
+New-VM -Name $name -ResourcePool $targetVMHost -Datastore TLB-Host04-DatastoreSSD -NumCpu 2 -MemoryMB 4096 -DiskGB 40 -NetworkName "DevLAN-EndPoints" -Floppy -cd -DiskStorageFormat Thin -GuestId 
